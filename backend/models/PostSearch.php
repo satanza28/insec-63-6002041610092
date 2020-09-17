45b03f6952a69f1191b4369a9574468b3fa10996
satanza28 <s6002041610092@email.kmutnb.ac.th>
2020-09-17 19:01:12 +0700

    Create PostSearch.php

---
+++ backend/models/PostSearch.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace backend\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use backend\models\Post;
+
+/**
+ * PostSearch represents the model behind the search form of `backend\models\Post`.
+ */
+class PostSearch extends Post
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'is_active', 'created_at', 'created_by', 'update_at'], 'integer'],
+            [['title', 'shot_description', 'description'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Post::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'is_active' => $this->is_active,
+            'created_at' => $this->created_at,
+            'created_by' => $this->created_by,
+            'update_at' => $this->update_at,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'shot_description', $this->shot_description])
+            ->andFilterWhere(['like', 'description', $this->description]);
+
+        return $dataProvider;
+    }
+}
